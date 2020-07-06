82bfd2620a4ecced57397cdcd0d3ffa31a2960b0
Jose-S <jose.sarvia99@gmail.com>
2020-07-06 11:31:49 -0500

    Inital Commit

--- src/DesignLanguage.ts
+++ src/DesignLanguage.ts
@@ -1,61 +1,76 @@
-import {Color, DropShadow, Image, Lottie, Toward, Typograph, Font, LinearGradient, Point2D, TextAlignment} from '@diez/prefabs';
-import {Margin} from './components/Margin';
-
+import {
+	Color,
+	DropShadow,
+	Image,
+	Lottie,
+	Toward,
+	Typograph,
+	Font,
+	LinearGradient,
+	Point2D,
+	TextAlignment,
+} from '@diez/prefabs';
+import { Margin } from './components/Margin';
+import { brandArchitectureTokens } from './designs/BrandArchitecture.figma';
 /**
  * You can collect anything inside a Diez component. Design tokens specified as
  * properties will be made available in the SDKs transpiled with Diez.
  */
 const colors = {
-  white: Color.hex('#FFFFFF'),
-  black: Color.hex('#000010'),
-  purple: Color.rgb(86, 35, 238),
-  darkPurple: Color.rgb(22, 11, 54),
-}
+	white: Color.hex('#FFFFFF'),
+	black: Color.hex('#00FF00'),
+	purple: brandArchitectureTokens.colors.harshOrange,
+	darkPurple: Color.rgb(22, 11, 54),
+};
 
 /**
  * You can reference properties from other components.
  */
 const palette = {
-  contentBackground: colors.white,
-  text: colors.black,
-  caption: colors.purple,
-  headerBackground: LinearGradient.make(Toward.Bottom, colors.darkPurple, colors.black),
-}
+	contentBackground: colors.white,
+	text: colors.black,
+	caption: colors.purple,
+	headerBackground: LinearGradient.make(
+		Toward.Bottom,
+		colors.darkPurple,
+		colors.black
+	),
+};
 
 /**
  * All of rich language features of TypeScript are at your disposal; for example,
  * you can define an object to keep track of your fonts.
  */
 const Fonts = {
-  SourceSansPro: {
-    Regular: Font.fromFile('assets/SourceSansPro-Regular.ttf'),
-  },
-}
+	SourceSansPro: {
+		Regular: Font.fromFile('assets/SourceSansPro-Regular.ttf'),
+	},
+};
 
 /**
  * Typographs encapsulate type styles with support for a specific font, font size,
  * and color. More typograph properties are coming soon.
  */
 const typography = {
-  heading1: new Typograph({
-    font: Fonts.SourceSansPro.Regular,
-    fontSize: 24,
-    color: palette.text,
-  }),
+	heading1: new Typograph({
+		font: Fonts.SourceSansPro.Regular,
+		fontSize: 24,
+		color: palette.text,
+	}),
 
-  body: new Typograph({
-    font: Fonts.SourceSansPro.Regular,
-    fontSize: 18,
-    color: palette.text,
-    alignment: TextAlignment.Center,
-  }),
+	body: new Typograph({
+		font: Fonts.SourceSansPro.Regular,
+		fontSize: 18,
+		color: palette.text,
+		alignment: TextAlignment.Center,
+	}),
 
-  caption: new Typograph({
-    font: Fonts.SourceSansPro.Regular,
-    fontSize: 14,
-    color: palette.caption,
-  }),
-}
+	caption: new Typograph({
+		font: Fonts.SourceSansPro.Regular,
+		fontSize: 14,
+		color: palette.caption,
+	}),
+};
 
 /**
  * In addition to colors and typography, you can also collect other types of
@@ -63,41 +78,43 @@ const typography = {
  * animations.
  */
 const images = {
-  logo: Image.responsive('assets/logo.png', 52, 48),
-  masthead: Image.responsive('assets/masthead.png', 208, 88),
-}
+	logo: Image.responsive('assets/logo.png', 52, 48),
+	masthead: Image.responsive('assets/masthead.png', 208, 88),
+};
 
 /**
  * You can even collect your own custom components.
  */
 const layoutValues = {
-  spacingSmall: 5,
-  spacingMedium: 25,
-  spacingLarge: 40,
-  contentMargin: new Margin({
-    top: 40,
-    left: 10,
-    right: 10,
-    bottom: 10,
-  }),
-}
+	spacingSmall: 5,
+	spacingMedium: 25,
+	spacingLarge: 40,
+	contentMargin: new Margin({
+		top: 40,
+		left: 10,
+		right: 10,
+		bottom: 10,
+	}),
+};
 
 /**
  * You can also define strings.
  */
 const strings = {
-  title: 'Diez',
-  caption: 'Keep your designs in sync with code',
-  helper: 'Modify the contents of “src/DesignLanguage.ts” (relative to the root of the Diez project) to see changes to the design language in real time.',
-}
+	title: 'Diez',
+	caption:
+		'Keep your designs in sync with code testing this out continue to try this out',
+	helper:
+		'Modify the contents of “src/DesignLanguage.ts” (relative to the root of the Diez project) to see changes to the design language in real time.',
+};
 
 const shadows = {
-  logo: new DropShadow({
-    offset: Point2D.make(0, 1),
-    radius: 16,
-    color: colors.black.fade(0.59),
-  }),
-}
+	logo: new DropShadow({
+		offset: Point2D.make(0, 1),
+		radius: 16,
+		color: colors.black.fade(0.59),
+	}),
+};
 
 /**
  * Note how this component is exported from `index.ts`. Diez compiles these
@@ -113,14 +130,14 @@ const shadows = {
  *   - If you run `yarn start android` or `npm run start android`, Diez will create an Android library.
  *     Look for `MainActivity.kt` inside `examples/android` to see how you can
  *     use Diez in an Android codebase.
-  *  - If you run `yarn start web` or `npm run start web`, Diez will create a Web App with your tokens.
+ *  - If you run `yarn start web` or `npm run start web`, Diez will create a Web App with your tokens.
  */
 export const designLanguage = {
-  palette,
-  typography,
-  images,
-  layoutValues,
-  strings,
-  shadows,
-  loadingAnimation: Lottie.fromJson('assets/loadingAnimation.json', false),
-}
+	palette,
+	typography,
+	images,
+	layoutValues,
+	strings,
+	shadows,
+	// loadingAnimation: Lottie.fromJson('assets/loadingAnimation.json', false),
+};
